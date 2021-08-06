702c27fba9e70e0c5721e522bbe322b5e4729ec5
Oleksandr Khoshaba <Oleksandr.Khoshaba@gmail.com>
2021-08-06 17:47:27 +0300

    Add write to file function

--- cload/src/main/java/org/vntu/cload/Benchmark.java
+++ cload/src/main/java/org/vntu/cload/Benchmark.java
@@ -1,5 +1,11 @@
 package org.vntu.cload;
 
+import java.io.File;
+import java.io.FileWriter;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
 public class Benchmark {
 
   private String address;
@@ -84,6 +90,31 @@ class RunBenchmark implements Runnable {
     }
 
 // For save to Disk:
+File file = new File("data.txt");
+//try {
+// create FileWriter object with file as parameter
+FileWriter outputfile = new FileWriter(file);
+  
+// create CSVWriter object filewriter object as parameter
+CSVWriter writer = new CSVWriter(outputfile);
+  
+// adding header to csv
+String[] header = { "Name", "Class", "Marks" };
+writer.writeNext(header);
+  
+// add data to csv
+String[] data1 = { "Aman", "10", "620" };
+
+writer.writeNext(data1);
+  
+// closing writer connection
+writer.close();
+// }
+
+//catch (IOException e) {
+//e.printStackTrace();
+//}
+
     }
     catch (Exception e)   {     // Throwing an exception
       System.out.println ("Exception is caught");
