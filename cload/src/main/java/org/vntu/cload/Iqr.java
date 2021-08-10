cbbadeceb20526a65dd507248e975713f2a6676d
Oleksandr Khoshaba <Oleksandr.Khoshaba@gmail.com>
2021-08-10 20:34:56 +0300

    Edit Iqr class

--- cload/src/main/java/org/vntu/cload/Iqr.java
+++ cload/src/main/java/org/vntu/cload/Iqr.java
@@ -40,40 +40,42 @@ class Iqr {
     }
   }
 
-  private List<Double> getOutliers(List<Double> input) {
-    List<Double> output = new ArrayList<Double>();
+  private List<Double> getOutliers(List<Double> inputDataIqr) {
+    List<Double> outputDataIqr = new ArrayList<Double>();
     List<Double> data1 = new ArrayList<Double>();
     List<Double> data2 = new ArrayList<Double>();
 
-        if (input.size() % 2 == 0) {
-            data1 = input.subList(0, input.size() / 2);
-            data2 = input.subList(input.size() / 2, input.size());
-        } else {
-            data1 = input.subList(0, input.size() / 2);
-            data2 = input.subList(input.size() / 2 + 1, input.size());
-        }
-        double q1 = getMedian(data1);
-        double q3 = getMedian(data2);
-        double iqr = q3 - q1;
-        double lowerFence = q1 - 1.5 * iqr;
-        double upperFence = q3 + 1.5 * iqr;
-        System.out.println("iqr:  " + iqr);
-        System.out.println("min:  " + input.get(0));
-        System.out.println("max:  " + input.get(input.size() - 1));
-        System.out.println("lowerFence:  " + lowerFence);
-        System.out.println("upperFence:  " + upperFence);
-        for (int i = 0; i < input.size(); i++) {
-            if (input.get(i) < lowerFence || input.get(i) > upperFence)
-                output.add(input.get(i));
-        }
-        return output;
+    if (inputDataIqr.size() % 2 == 0) {
+      data1 = inputDataIqr.subList(0, inputDataIqr.size() / 2);
+      data2 = inputDataIqr.subList(inputDataIqr.size() / 2, inputDataIqr.size());
+    } else {
+      data1 = inputDataIqr.subList(0, inputDataIqr.size() / 2);
+      data2 = inputDataIqr.subList(inputDataIqr.size() / 2 + 1, inputDataIqr.size());
     }
 
-    private double getMedian(List<Double> data) {
-        if (data.size() % 2 == 0)
-            return (data.get(data.size() / 2) + data.get(data.size() / 2 - 1)) / 2;
-        else
-            return data.get(data.size() / 2);
+    double q1 = getMedian(data1);
+    double q3 = getMedian(data2);
+    double iqr = q3 - q1;
+    double lowerFence = q1 - 1.5 * iqr;
+    double upperFence = q3 + 1.5 * iqr;
+
+    System.out.println("iqr:  " + iqr);
+    System.out.println("min:  " + inputDataIqr.get(0));
+    System.out.println("max:  " + inputDataIqr.get(inputDataIqr.size() - 1));
+    System.out.println("lowerFence:  " + lowerFence);
+    System.out.println("upperFence:  " + upperFence);
+    for (int count = 0; count < inputDataIqr.size(); count++) {
+      if (inputDataIqr.get(count) < lowerFence || inputDataIqr.get(count) > upperFence)
+         outputDataIqr.add(inputDataIqr.get(count));
+      }
+    return outputDataIqr;
+  }
+
+  private double getMedian(List<Double> data) {
+    if (data.size() % 2 == 0)
+      return (data.get(data.size() / 2) + data.get(data.size() / 2 - 1)) / 2;
+    else
+      return data.get(data.size() / 2);
     }
 }
 
