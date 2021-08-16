6b48a3477214a5c76583e2da11f13bc6f4d3c442
Oleksandr Khoshaba <Oleksandr.Khoshaba@gmail.com>
2021-08-16 09:22:33 +0300

    Edit class BenchmarkApplication

--- cload/src/main/java/org/vntu/cload/BenchmarkApplication.java
+++ cload/src/main/java/org/vntu/cload/BenchmarkApplication.java
@@ -4,9 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+* The class Benchmark application represents the HPCResearching Program Environment
+* @author Oleksandr Khoshaba
+*/ 
 public class BenchmarkApplication {
 
-	public static void main(String[] args) {
+/** 
+ *
+ * The method Main calls the class {@link BenchmarkApplication}
+ *
+ * @param args  the args for input {@link String}
+*/
+  public static void main(String[] args) {
 		SpringApplication.run(BenchmarkApplication.class, args);
 	}
 
